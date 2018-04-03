dc786e80a8a6ab769d70029fb8ec9794268cf8ff
Riley Mathews <rileymathews80@gmail.com>
2018-04-03 13:46:06 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+//app to check for last name length
+const lastName = "mathews";
+
+//check name length
+if (lastName.length >= 7) {
+    console.log("I can't pronounce this last name");
+} else {
+    console.log(lastName);
+}
